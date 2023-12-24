d97930c557996d5de1bdaa8114ad40f68fa23f96
Stoyan83 <stoyan_stoyanov@rocketmail.com>
2023-12-24 15:09:48 +0200

    Logic for unique products

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,25 @@ class Product < ApplicationRecord
   belongs_to :size, optional: true
   belongs_to :brand
 
-  validates_uniqueness_of :product_group_id, scope: [:color_id, :size_id, :brand_id], allow_nil: true
   validates_presence_of :product_group
   validates_presence_of :brand
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
   validates :base_price, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
+  validate :unique_combination_of_attributes
+
+  private
+
+  def unique_combination_of_attributes
+    existing_record = Product.find_by(
+      product_group_id:,
+      color_id: color_id || nil,
+      size_id: size_id || nil,
+      brand_id:
+    )
+
+    return unless existing_record && existing_record != self
+
+    errors.add(:base, "Product with the same color, size, brand, and product group already exists. (ID: #{existing_record.id})")
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,16 +5,39 @@ require 'rails_helper'
 describe Product do
   context 'associations' do
     it { is_expected.to belong_to(:product_group) }
+    it { is_expected.to belong_to(:brand) }
     # it { is_expected.to belong_to(:color).optional }
     # it { is_expected.to belong_to(:size).optional }
-    it { is_expected.to belong_to(:brand) }
   end
 
   context 'validations' do
     it { is_expected.to validate_presence_of(:product_group) }
     it { is_expected.to validate_presence_of(:brand) }
-
     it { is_expected.to validate_numericality_of(:quantity).only_integer.is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_numericality_of(:base_price).only_integer.is_greater_than_or_equal_to(0) }
+
+    describe '#unique_combination_of_attributes' do
+      let(:existing_product) { create(:product) }
+
+      it 'validates uniqueness of the combination of attributes' do
+        product = build(:product,
+                        product_group: existing_product.product_group,
+                        # color: existing_product.color,
+                        # size: existing_product.size,
+                        brand: existing_product.brand)
+
+        expect(product).not_to be_valid
+        expect(product.errors[:base]).to include(
+          "Product with the same color, size, brand, and product group already exists. (ID: #{existing_product.id})"
+        )
+      end
+
+      it 'does not add errors if the combination is unique' do
+        product = build(:product)
+
+        expect(product).to be_valid
+        expect(product.errors[:base]).to be_empty
+      end
+    end
   end
 end
